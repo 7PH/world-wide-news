45cca34819f6dac71eea5896424b35351e6ab077
PowerHat <b.raymond@protonmail.com>
2018-10-11 17:00:52 +0200

    End build process for views

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,30 @@
+const gulp = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const pug = require('gulp-pug');
 
+
+/**
+ *
+ * @type {{pug: {pages: string[]}, dest: string}}
+ */
+const CONFIG = {
+    pug: {
+        pages: ['app/view/**/*'],
+    },
+    dest: 'docs/'
+};
 
 
 
 // @TODO build process
 // @TODO deploy process
+
+
+
+
+gulp.task("copy-views", () => {
+    return gulp.src(CONFIG.pug.pages)
+        .pipe(pug())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(CONFIG.dest))
+});
