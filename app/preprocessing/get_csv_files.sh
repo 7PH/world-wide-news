522818ff2c2f1874a70d1f413d7130d2a764fa3c
PowerHat <b.raymond@protonmail.com>
2018-11-27 10:52:46 +0100

    Refactor bash script & Create data folder

--- app/preprocessing/get_csv_files.sh
+++ app/preprocessing/get_csv_files.sh
@@ -7,8 +7,9 @@
 # Author: Virginie Piskin
 # ------------------------------------------------
 
-SOURCE=http://data.gdeltproject.org/gdeltv2/masterfilelist.txt
-TARGET=./
+SOURCE="http://data.gdeltproject.org/gdeltv2/masterfilelist.txt";
+TARGET="./data";
+MASTER_FILE="masterfilelist.txt";
 
 # Print usage by default
 if [[ $# -lt 1 || $1 == "h" || $1 == "-help" || $1 == "--help" ]] ; then
@@ -33,7 +34,6 @@ fi
 # Read the options
 while getopts "y:m:d:h:N:" opt; do
 	case $opt in
-	
         	y)
 			year=$OPTARG
 			echo "$year"
@@ -51,9 +51,9 @@ while getopts "y:m:d:h:N:" opt; do
 			echo "$hour"
 			;;
 		N)
-                        N=$OPTARG
-                        echo "$N"
-                        ;;
+            N=$OPTARG
+            echo "$N"
+            ;;
 
 		\?) echo "Unknown option $OPTARG"; exit 2;;
 
@@ -61,15 +61,11 @@ while getopts "y:m:d:h:N:" opt; do
 	esac
 done
 
-master_file=masterfilelist.txt
-
-wget $SOURCE -O $TARGET/masterfilelist.txt
-
-min_year=2015 ; max_year=2018
-min_month=1 ; max_month=12
-min_day=1 ; max_day=31
-min_hour=0 ; max_hour=23
-min_min=0 ; max_min=60
+min_year=2015; max_year=2018;
+min_month=1; max_month=12;
+min_day=1; max_day=31;
+min_hour=0; max_hour=23;
+min_min=0; max_min=60;
 
 year_list=$(python -c "print(range($min_year, $min_year + $year, 1))")
 month_list=$(python -c "print(range($min_month, $max_month +1, $max_month/$month))")
@@ -77,46 +73,56 @@ day_list=$(python -c "print(range($min_day, $max_day + 1, $max_day/$day))")
 hour_list=$(python -c "print(range($min_hour, $max_hour, $max_hour/$hour))")
 min_list=$(python -c "print(range($min_min, $max_min, $max_min/$N))")
 
-echo "$year_list \n $month_list \n $day_list \n $hour_list \n $min_list"
+# Init data folder
+mkdir -p "$TARGET";
 
+# Go fetch master file
+wget "$SOURCE" -O "$TARGET/$MASTER_FILE";
+
+# Years
 for year in $year_list ; do
 	y=$(echo $year | sed -e 's/[][,]//g')
+
+	# Months
 	for month in $month_list ; do
 		m=$(echo $month | sed -e 's/[][,]//g')
 		count=$(echo -n $m | wc -c)
 		if [ $count == 1 ] ; then 
 			m=$(echo "0$m")
 		fi
-		for day in $day_list ; do 
+
+		# Days
+		for day in $day_list ; do
 			d=$(echo $day | sed -e 's/[][,]//g')
 			count=$(echo -n $d | wc -c)
 			if [ $count == 1 ] ; then
 				d=$(echo "0$d")
 			fi
-			for hour in $hour_list ; do 
+
+			# Hours
+			for hour in $hour_list ; do
 				h=$(echo $hour | sed -e 's/[][,]//g')
-                        	count=$(echo -n $h | wc -c)
-                        	if [ $count == 1 ] ; then
-                                	h=$(echo "0$h")
-                        	fi
-				for min in $min_list ; do 
+                count=$(echo -n $h | wc -c)
+                if [ $count == 1 ] ; then
+                        h=$(echo "0$h")
+                fi
+
+				# ??? @TODO document
+				for min in $min_list ; do
 					m=$(echo $min | sed -e 's/[][,]//g')
-	                                count=$(echo -n $m | wc -c)
-        	                        if [ $count == 1 ] ; then
-                	                        m=$(echo "0$m")
-                        	        fi
-                                	echo "in $y$m$d$h$m"
-					cat $TARGET | awk '{print $8}' | grep $y$m$d$h$m | grep export
-					exp=$(cat $TARGET | awk '{print $3}' | grep $y$m$d$h$m | grep export)
-					gkg=$(cat $TARGET | awk '{print $3}' | grep $y$m$d$h$m | grep gkg)
-					men=$(cat $TARGET | awk '{print $3}' | grep $y$m$d$h$m | grep mentions)
+                    count=$(echo -n $m | wc -c)
+                    if [ $count == 1 ] ; then
+                            m=$(echo "0$m")
+                    fi
+
+					cat "$TARGET/$MASTER_FILE" | awk '{print $8}' | grep $y$m$d$h$m | grep export;
+					exp=$(cat "$TARGET/$MASTER_FILE" | awk '{print $3}' | grep $y$m$d$h$m | grep export)
+					gkg=$(cat "$TARGET/$MASTER_FILE" | awk '{print $3}' | grep $y$m$d$h$m | grep gkg)
+					men=$(cat "$TARGET/$MASTER_FILE" | awk '{print $3}' | grep $y$m$d$h$m | grep mentions)
 					if [[ exp != '' ]] && [[ gkg != '' ]] && [[ men != '' ]] ; then
-						#wget $exp -P $TARGET
-						#wget $gkg -P $TARGET
-						#wget $gkg -P $TARGET
-						echo $exp >> export.txt
-						echo $gkg >> gkg.txt
-						echo $men >> mentions.txt
+						echo $exp >> "$TARGET/export.txt";
+						echo $gkg >> "$TARGET/gkg.txt";
+						echo $men >> "$TARGET/mentions.txt";
 					fi 
 				done
 			done
---
+++ data/README.md
@@ -0,0 +1,1 @@
+This folder contains the required data for the visualization
